285b36e297a39a1df59dcd6898eec6fedb5e1c6c
hhhbbbhhh <haroonb@gmail.com>
2020-11-30 18:58:49 +0500

    added movies dao and updated dependencies.

--- data/src/main/java/com/example/data/db/dao/MoviesDao.java
+++ data/src/main/java/com/example/data/db/dao/MoviesDao.java
@@ -1,4 +1,27 @@
 package com.example.data.db.dao;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import com.example.data.db.entities.Movie;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+
+@Dao
 public interface MoviesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable insertMovie(Movie movie);
+
+    @Query("select * from movies")
+    Observable<List<Movie>> getPlayNowList();
+
+    @Query("Delete from movies")
+    void clearMovies();
+
 }
